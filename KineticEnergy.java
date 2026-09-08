78deae75d725bd19a84f93df8fdbfd5f994f5cf7
LeoDeveci <leonardodeveci1210@gmail.com>
2026-09-08 14:41:43 -0400

    Progress update 1, not completed yet

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,14 @@
+Scanner input = new Scanner(System.in);
 public class KineticEnergy {
-
+    int Velocity;
+    int Mass;
+    int KeneticE;
+    public KineticEnergy(int V, int M, int K){
+        Velocity = V;
+        Mass = M;
+        KeneticE = K;
+}
+System.out.print("input Mass:");
+Mass = input.nextInt; 
+System.out.println("input Velocity");
 }
